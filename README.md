9b207d6c65ecd4c21420432a04038543f7ac7af6
avaneesh23 <avaneesh.murthy@gmail.com>
2015-11-30 02:11:40 -0500

    Location based search

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Installations required:
 2. WebStorm or any other IDE of your interest from https://www.jetbrains.com/webstorm/download/
 3. MongoDb from https://www.mongodb.org/downloads#production
 4. Run npm install express
-5. start the MongoDB server from C:\Program Files\MongoDB\Server\3.0\bin\mongod.exe
 
 Executing the code on localhost:
 
@@ -18,9 +17,30 @@ Steps 5, 6 and 7 are only for the first time
 Client side code: /public
 Server side code: /server.js
 
+Videos to watch:
 
-You should be able to view the list of events as soon as you visit the location. 
-If you are unable to have that view then please follow these steps to add an extension to your chrome:
-1. Settings->extensions->get more extensions
-2. Search for cors. Select the extension with name 'Allow-control-allow-origin'
-3. Make sure the CORS icon appears green on your browser else please enable it.
+Setup
+http://portal-cs5610online.rhcloud.com/portal/index.html#/modules/1/lectures
+
+From : 1:37:03
+https://plus.google.com/events/cf3armvkiflhg34a0k79v9252rc
+
+From 47:00
+https://plus.google.com/events/c8i32bus758m36pl3mk303ktfis
+
+Node:
+http://portal-cs5610online.rhcloud.com/portal/index.html#/modules/4
+
+https://plus.google.com/events/c2ag5h3rms3oror8b88ssq2mh7o
+
+https://plus.google.com/events/cb47806njik9gsrp0cg60e5dmmo
+
+https://plus.google.com/events/cf4al210ec4kq687e6o2fsjs5f0
+
+https://plus.google.com/events/cgei6ntufccbupnpbk22g0sf0go
+
+https://plus.google.com/events/civnkq93odn37s2masv82k6amic
+
+https://plus.google.com/events/cor39bvlamia7uid9peiira1mgk
+
+https://plus.google.com/events/c21kkrvcfqdoapitqe1chki457g
--- public/script.js
+++ public/script.js
@@ -44,31 +44,23 @@ scotchApp.controller('getLocation', function ($scope, $http, NgMap) {
 
     //google.maps.event.trigger(map, "resize");
     $scope.gPlace,
-<<<<<<< HEAD
+        $scope.eventDetails = [],
+        $scope.busy = false,
+        $scope.loader = true,
+        $scope.pageNo = 1,
+        $scope.pageCount = 0,
         $scope.userlat = "0",
         $scope.userlong = "0",
         $scope.error = "",
         $scope.visibility = false,
         $scope.searchQuery = "",
         $scope.category = "",
-        $scope.sortOptions = ["Popularity", "Date", "Relevance"],
+        $scope.sortOptions = ["Popularity","Date","Relevance"],
         $scope.sortOrder = "Popularity",
         $scope.where = "",
+        $scope.clickedEvent = null,
         $scope.apiKey = "rcnxbzfT3dLNF3ff",
         $scope.url = "";
-=======
-    $scope.userlat = "0",
-    $scope.userlong = "0",
-    $scope.error = "",
-    $scope.visibility = false,
-    $scope.searchQuery = "",
-    $scope.category = "",
-    $scope.sortOptions = ["Popularity","Date","Relevance"],
-    $scope.sortOrder = "Popularity",
-    $scope.where = "",
-    $scope.apiKey = "rcnxbzfT3dLNF3ff",
-    $scope.url = "";
->>>>>>> origin/master
     //  $scope.url = "http://api.eventful.com/json/events/search?app_key="+$scope.apiKey+"&keywords=books&location="+$scope.searchQuery+"&date=Future";
 
     $scope.init = function () {
@@ -85,7 +77,7 @@ scotchApp.controller('getLocation', function ($scope, $http, NgMap) {
         $scope.userlat = position.coords.latitude;
         $scope.userlong = position.coords.longitude;
         $scope.where = $scope.userlat + "," + $scope.userlong;
-        $scope.url = "http://api.eventful.com/json/events/search?app_key=" + $scope.apiKey + "&category=" + $scope.category.id + "&where=" + $scope.where + "&within=10&units=mi&date=Future&page_size=50&include=categories,price,links&sort_order=" + $scope.sortOrder;
+        //$scope.url = "http://api.eventful.com/json/events/search?app_key=" + $scope.apiKey + "&category=" + $scope.category.id + "&where=" + $scope.where + "&within=10&units=mi&date=Future&page_size=50&include=categories,price,links&sort_order=" + $scope.sortOrder;
         $scope.categoryUrl = "http://api.eventful.com/json/categories/list?app_key=" + $scope.apiKey;
         $scope.showCategories();
         $scope.show();
@@ -103,6 +95,9 @@ scotchApp.controller('getLocation', function ($scope, $http, NgMap) {
         });
     };
 
+    $scope.showEvent = function (event) {
+        $scope.clickedEvent = event;
+    };
 
     $scope.showError = function (error) {
         switch (error.code) {
@@ -133,7 +128,6 @@ scotchApp.controller('getLocation', function ($scope, $http, NgMap) {
         $scope.map.showInfoWindow('map-event');
     };
 
-<<<<<<< HEAD
     $scope.hideDetail = function () {
         $scope.map.hideInfoWindow('map-event');
     };
@@ -141,21 +135,44 @@ scotchApp.controller('getLocation', function ($scope, $http, NgMap) {
     $scope.search = function () {
         //alert("searching..");
         $scope.where = $scope.searchQuery;
-        if ($scope.where == "")
+        if($scope.where == "")
             $scope.where = $scope.userlat + "," + $scope.userlong;
 
+        $scope.eventDetails.length = 0;
+        $scope.pageNo = 1;
+        $scope.loader = true;
+        if($scope.category == null)
+            $scope.category = "";
         //alert(typeof $scope.searchQuery);
-        $scope.url = "http://api.eventful.com/json/events/search?app_key=" + $scope.apiKey + "&category=" + $scope.category.id + "&where=" + $scope.where + "&within=10&units=mi&date=Future&page_size=50&include=categories,price,links&sort_order=" + $scope.sortOrder;
+        //$scope.url = "http://api.eventful.com/json/events/search?app_key=" + $scope.apiKey + "&category=" + $scope.category.id + "&where=" + $scope.where + "&within=10&units=mi&date=Future&page_size=50&include=categories,price,links&sort_order=" + $scope.sortOrder;
         //alert($scope.url);
         $scope.show();
     };
 
+    $scope.more = function(){
+        //alert($scope.pageNo + " " + $scope.pageCount);
+        if($scope.pageNo<$scope.pageCount){
+            //alert($scope.pageNo + " " + $scope.pageCount);
+            if($scope.busy) return;
+            $scope.pageNo = $scope.pageNo + 1;
+            $scope.busy = true;
+            $scope.show();
+        }
+    };
+
     $scope.show = function () {
+        //$scope.loader = true;
         $scope.visibility = true;
+        //$scope.eventDetails = [];
+        //alert($scope.pageNo);
+        $scope.url = "http://api.eventful.com/json/events/search?app_key=" + $scope.apiKey + "&category=" + $scope.category.id + "&where=" + $scope.where + "&within=10&units=mi&date=Future&page_size=10&page_number=" + $scope.pageNo + "&include=categories,price,links&sort_order=" + $scope.sortOrder;
         $http.get($scope.url).success(function (data, status, headers, config) {
 
             $scope.eventData = data;
-            $scope.eventDetails = [];
+            //alert(JSON.stringify($scope.eventData));
+
+            $scope.pageCount = $scope.eventData.page_count;
+            //alert($scope.pageCount);
             //alert($scope.eventData.events.event.length);
             //  for(var i=1; i<$scope.eventData.page_count;i++)
             //  {
@@ -164,7 +181,7 @@ scotchApp.controller('getLocation', function ($scope, $http, NgMap) {
                 eventObj.url = $scope.eventData.events.event[j].url;
                 eventObj.title = $scope.eventData.events.event[j].title;
                 eventObj.desc = $scope.eventData.events.event[j].description;
-                if (eventObj.desc == null)
+                if(eventObj.desc == null)
                     eventObj.desc = "There is no description for this event."
                 eventObj.start_time = $scope.eventData.events.event[j].start_time;
                 eventObj.stop_time = $scope.eventData.events.event[j].stop_time;
@@ -175,66 +192,8 @@ scotchApp.controller('getLocation', function ($scope, $http, NgMap) {
                 eventObj.longitude = $scope.eventData.events.event[j].longitude;
                 eventObj.image = "images/default_image.png";
                 eventObj.price = "Free";
-                if ($scope.eventData.events.event[j].price != null) {
+                if($scope.eventData.events.event[j].price != null){
                     eventObj.price = "$ " + $scope.eventData.events.event[j].price;
-=======
-        $scope.clickEventInfo = function (event, e) {
-            //alert("here"+JSON.stringify(e));
-            $scope.mapEvent = e;
-            $scope.map.showInfoWindow('map-event');
-        };
-
-        $scope.hideDetail = function () {
-            $scope.map.hideInfoWindow('map-event');
-        };
-
-        $scope.search = function () {
-            //alert("searching..");
-            $scope.where = $scope.searchQuery;
-            if($scope.where == "")
-                $scope.where = $scope.userlat + "," + $scope.userlong;
-
-            //alert(typeof $scope.searchQuery);
-            $scope.url = "http://api.eventful.com/json/events/search?app_key=" + $scope.apiKey + "&category=" + $scope.category.id + "&where=" + $scope.where + "&within=10&units=mi&date=Future&page_size=50&include=categories,price,links&sort_order=" + $scope.sortOrder;
-            //alert($scope.url);
-            $scope.show();
-        };
-
-        $scope.show = function () {
-            $scope.visibility = true;
-            $http.get($scope.url).success(function (data, status, headers, config) {
-
-                $scope.eventData = data;
-                $scope.eventDetails = [];
-                //alert($scope.eventData.events.event.length);
-                //  for(var i=1; i<$scope.eventData.page_count;i++)
-                //  {
-                for (var j = 0; j < $scope.eventData.events.event.length; j++) {
-                    var eventObj = new Object();
-                    eventObj.url = $scope.eventData.events.event[j].url;
-                    eventObj.title = $scope.eventData.events.event[j].title;
-                    eventObj.desc = $scope.eventData.events.event[j].description;
-                    if(eventObj.desc == null)
-                        eventObj.desc = "There is no description for this event."
-                    eventObj.start_time = $scope.eventData.events.event[j].start_time;
-                    eventObj.stop_time = $scope.eventData.events.event[j].stop_time;
-                    eventObj.venue_name = $scope.eventData.events.event[j].venue_name;
-                    eventObj.venue_address = $scope.eventData.events.event[j].venue_address;
-                    eventObj.city = $scope.eventData.events.event[j].city_name;
-                    eventObj.latitude = $scope.eventData.events.event[j].latitude;
-                    eventObj.longitude = $scope.eventData.events.event[j].longitude;
-                    eventObj.image = "images/default_image.png";
-                    eventObj.price = "Free";
-                    if($scope.eventData.events.event[j].price != null){
-                        eventObj.price = "$ " + $scope.eventData.events.event[j].price;
-                    }
-                    if ($scope.eventData.events.event[j].image != null) {
-                        eventObj.image = $scope.eventData.events.event[j].image.medium.url;
-                    }
-                    eventObj.categories = $scope.eventData.events.event[j].categories.category;
-
-                    $scope.eventDetails.push(eventObj);
->>>>>>> origin/master
                 }
                 if ($scope.eventData.events.event[j].image != null) {
                     eventObj.image = $scope.eventData.events.event[j].image.medium.url;
@@ -244,6 +203,8 @@ scotchApp.controller('getLocation', function ($scope, $http, NgMap) {
                 $scope.eventDetails.push(eventObj);
             }
             //  }
+            $scope.busy = false;
+            $scope.loader = false;
         }).error(function (error) {
         });
     };
@@ -256,30 +217,18 @@ scotchApp.filter('ampersand', function () {
     }
 });
 
-<<<<<<< HEAD
 scotchApp.directive('googleplace', function () {
     return {
         require: 'ngModel',
         link: function (scope, element, attrs, model) {
-=======
-scotchApp.directive('googleplace', function() {
-    return {
-        require: 'ngModel',
-        link: function(scope, element, attrs, model) {
->>>>>>> origin/master
             var options = {
                 types: [],
                 componentRestrictions: {}
             };
             scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
 
-<<<<<<< HEAD
             google.maps.event.addListener(scope.gPlace, 'place_changed', function () {
                 scope.$apply(function () {
-=======
-            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
-                scope.$apply(function() {
->>>>>>> origin/master
                     model.$setViewValue(element.val());
                 });
             });
@@ -287,13 +236,20 @@ scotchApp.directive('googleplace', function() {
     };
 });
 
-<<<<<<< HEAD
 scotchApp.filter('dateInMillis', function () {
     return function (dateString) {
-=======
-scotchApp.filter('dateInMillis', function() {
-    return function(dateString) {
->>>>>>> origin/master
         return Date.parse(dateString);
+    };
+});
+
+scotchApp.directive("scrollend", function() {
+    return function(scope, element, attrs) {
+        var container = angular.element(element);
+        container.bind("scroll", function(evt) {
+            if (container[0].offsetHeight + container[0].scrollTop >= container[0].scrollHeight) {
+                //alert('On the bottom of the world I\'m waiting.');
+                scope.$apply("more()");
+            }
+        });
     };
 });
