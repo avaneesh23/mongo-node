7bdf443cda20d049ae4bfdbe2b047439800915b9
avaneesh23 <avaneesh.murthy@gmail.com>
2015-12-17 03:14:41 -0500

    clearing up for updates

--- public/myAccCtrl.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by RavitejaSomisetty on 11/26/2015.
- */
-scotchApp.controller("myAccCtrl", function ($scope, $location, MyService, $rootScope) {
-    $scope.firstName=$rootScope.currentUser.firstname;
-    $scope.lastName=$rootScope.currentUser.lastname;
-    $scope.email=$rootScope.currentUser.email;
-});
